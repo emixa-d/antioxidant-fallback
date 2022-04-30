6c301b1dc8f892497f85be6325b595c3f166ebba
Maxime Devos <maximedevos@telenet.be>
2022-04-30 09:56:14 +0000

    Avoid non-building crate.

--- guix.scm
+++ guix.scm
@@ -1035,6 +1035,7 @@ of operation.")
 ;; TODO: write a "guix style" thing for doing this.
 (define %removed-dependencies
   '("rust-quickcheck" ; (quickcheck env-logger humantime chrono bincode) cycle
+    "rust-rustc-test" ; doesn't build against recent rust-time
     "rust-speculate" ; @0.1.2 doesn't build against recent rust-syn
     "rust-skeptic" ; @0.13.4 doesn't build
     "rust-boxxy" ; doesn't build and not supposed to be used ‘in production’
