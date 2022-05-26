012ffb24587dbccbb3e64e3508516d530b055ca8
Maxime Devos <maximedevos@telenet.be>
2022-05-26 13:06:57 +0000

    Make a channel entry.

---
+++ news.scm
@@ -0,0 +1,8 @@
+(channel-news
+ (version 0)
+ (entry (title (en "Feature variants of packages now supported"))
+	(body (en "There now is some support for declaring multiple variants
+of the same crate with a different set of features.  Theoretically, it should
+now be possible to link multiple versions of the same thing into the same
+binary, reducing the number of version conflicts (untested).  See README.md
+for details."))))
