69790847542ecccc10ed06dd4c7e262b60cf98d4
Maxime Devos <maximedevos@telenet.be>
2022-05-31 11:06:20 +0000

    Add missing example

---
+++ shared-lib/example.c
@@ -0,0 +1,7 @@
+#include <stddef.h>
+
+extern size_t get_life();
+
+int main(void) {
+  return (size_t) get_life();
+}
---
+++ shared-lib/lib.rs
@@ -0,0 +1,5 @@
+#[no_mangle]
+pub unsafe extern "C" fn get_life() -> isize {
+    return 42;
+}
+
