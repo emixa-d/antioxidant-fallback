7d34a8244d4e822a2652a749d53089ab5edf1c8d
Maxime Devos <maximedevos@telenet.be>
2022-06-19 12:34:54 +0000

    antioxidant-ci: Show intermediate packages as well.
    Seems convenient for tracking process.  Also: more substitutes.
    
    - define package-closure/vhash
    - define rusty-package-closure
    - apply rusty-package-closure to (all-packages)

--- antioxidant-ci.scm
+++ antioxidant-ci.scm
@@ -16,11 +16,15 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with Antioxidant.  If not, see <http://www.gnu.org/licenses/>.
 (define-module (antioxidant-ci)
-  #:use-module ((antioxidant-packages) #:select (vitaminate/auto public-test-package))
+  #:use-module ((antioxidant-packages) #:select (vitaminate/auto public-test-package
+								 antioxidant-build-system))
   #:use-module ((guix build-system cargo) #:select (cargo-build-system))
   #:use-module ((guix profiles) #:select (packages->manifest))
+  #:use-module ((guix packages) #:select (package? package-direct-inputs))
   #:use-module ((gnu packages) #:select (fold-packages))
   #:use-module ((rnrs exceptions) #:select (guard))
+  #:use-module (ice-9 vlist)
+  #:use-module (srfi srfi-1)
   #:use-module ((guix packages) #:select (package-build-system package-name))
   #:autoload (guix ui) (warning G_)
   #:export (all-packages))
@@ -50,7 +54,39 @@
 		(public-test-package (vitaminate/auto foo))) list)))
   (fold-packages add '() #:select? is-leaf-cargo-rust-package?))
 
+(define (package-closure/vhash todo seen descend?)
+  ;; Compute the closure, depth-first
+  ;; todo: list of packages
+  ;; seen: vhash -> #true of packages
+  (define (only-package input)
+    (second input))
+  (define (proc new-package seen)
+    (cond ((vhash-assq new-package seen) ; already visited
+	   seen)
+	  ((descend? new-package)
+	   (package-closure/vhash
+	    (map only-package (package-direct-inputs new-package))
+	    (vhash-consq new-package #true seen)
+	    descend?))
+	  (#true
+	   (vhash-consq new-package #true seen))))
+  (fold proc seen todo))
+
+(define (rusty-package-closure packages)
+  (define (vhash->key-list vhash)
+    (define (cons-it key _ rest)
+      (cons key rest))
+    (vhash-fold cons-it '() vhash))
+  (define (descend? package)
+    (and (package? package) ; could be an <origin>
+	 (or (string-prefix? "antioxidated-" (package-name package))
+	     (string-prefix? "rust-" (package-name package))
+	     (eq? (package-build-system package) antioxidant-build-system))))
+  (vhash->key-list (package-closure/vhash packages vlist-null descend?)))
+
 ;; The idea is to build all packages in (all-packages) by the CI infrastructure.
 ;; Apparently returning a manifest is convenient for the CI infrastructure, see
 ;; see <https://github.com/emixa-d/antioxidant-fallback/pull/1>.
-(packages->manifest (all-packages))
+;;
+;; By using rusty-package-closure, the intermediate packages are shown as well.
+(packages->manifest (rusty-package-closure (all-packages)))
