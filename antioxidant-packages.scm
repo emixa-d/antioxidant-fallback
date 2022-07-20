939fc09393bcecda22ddc3381104d6617bf9c934
Maxime Devos <maximedevos@telenet.be>
2022-07-20 16:37:30 +0200

    rust-log: Disable tests with removed dependency rust-serde-test.
    * antioxidant-packages.scm (%disable-tests)[rust-log]: New entry.

--- antioxidant-packages.scm
+++ antioxidant-packages.scm
@@ -5129,6 +5129,7 @@ RFC-compliant `EmailAddress` newtype. ")
 (define %disable-tests
   '(("rust-atty" . build-environment) ; assumes fd 0/1/2 are ttys
     ("rust-erased-serde" . removed-dependency) ; rust-serde-json
+    ("rust-log" . removed-dependency) ; rust-serde-test
     ("rust-serde-fmt" . removed-dependency) ; rust-serde-derive
     ("rust-sval" . removed-dependency))) ; quickcheck
 
