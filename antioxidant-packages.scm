146aa1e228c42b3a3e659a93458c301fd35ea8ba
Maxime Devos <maximedevos@telenet.be>
2022-07-30 17:18:03 +0200

    antioxidant: Don't skip rust-unsafe-unwrap tests.
    Debug assertions are now enabled by default, so they shouldn't be
    required anymore.
    
    * antioxidant-packages.scm (%test-options)[rust-unsafe-unwrap]:
    Remove.

--- antioxidant-packages.scm
+++ antioxidant-packages.scm
@@ -5260,12 +5260,7 @@ RFC-compliant `EmailAddress` newtype. ")
      "--skip=test::check_system_info") ; there's no /etc/os-release or /etc/lsb-release (TODO: maybe we could patch SystemExt.name to just return Guix?).
     ("rust-tiff"
      "--exact"
-     "--skip=encoder::compression::deflate::tests::test_deflate") ; <https://github.com/image-rs/image-tiff/issues/169>
-    ("rust-unsafe-unwrap"
-     "--exact"
-     ;; the _failure tests assume that 'debug_assertions' is used
-     "--skip=tests::option_unwrap_failure"
-     "--skip=tests::result_unwrap_failure")))
+     "--skip=encoder::compression::deflate::tests::test_deflate"))) ; <https://github.com/image-rs/image-tiff/issues/169>
 
 ;; Packages for which tests are disabled.
 ;; The second part of the pair is a 'reason' for disabling them.
